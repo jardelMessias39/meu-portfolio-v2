754590dc06affbec56d06cd2c7acef0509171255
Jardel Messias <jardelmessias28@gmail.com>
2025-09-06 03:53:42 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{padding: '50px', textAlign: 'center'}}>
+      <h1>Jardel Messias - Desenvolvedor Full Stack</h1>
+      <p>Portfolio em construção...</p>
+    </div>
+  );
+}
+
+export default App;
